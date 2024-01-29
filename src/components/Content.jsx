dccc366955bee76036bf7d6f4a8ab665e467a57c
oebaedz <oebaedz@gmail.com>
2024-01-30 01:37:43 +0700

    fixed url for fetching

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -5,7 +5,7 @@ const Content = () => {
     const [city, setCity] = useState("Probolinggo")
     const [data, setData] = useState({})
 
-    const url = `http://api.aladhan.com/v1/timingsByAddress?address=${city}`
+    const url = `https://api.aladhan.com/v1/timingsByAddress?address=${city}`
     const ConvertToArabicNumbers = (num) => {
         const arabicNumbers = '\u0660\u0661\u0662\u0663\u0664\u0665\u0666\u0667\u0668\u0669';
         return new String(num).replace(/[0123456789]/g, (d) => { return arabicNumbers[d] });
@@ -20,13 +20,13 @@ const Content = () => {
         <div className='flex flex-col md:px-64'>
             <div className='flex flex-col gap-4 my-4 text-center items-center justify-center md:justify-between md:flex-row'>
                 <div>
-                    <p className="font-light">{data.meta && data.meta.method.name}</p>
-                    <p className="font-medium text-xl">{city}</p>
+                    <p className="font-light text-xs">{data.meta && data.meta.method.name}</p>
+                    <p className="font-medium text-lg">{city}</p>
                 </div>
                 <div>
-                    <p className="font-medium text-xl">{data.date && data.date.hijri.weekday.ar}</p>
-                    <p className="font-light text-xl">{data.date && `${ConvertToArabicNumbers(data.date.hijri.day)} ${data.date.hijri.month.ar} ${ConvertToArabicNumbers(data.date.hijri.year)}`}</p>
-                    <p className="font-light text-xl">{data.date && data.date.readable}</p>
+                    <p className="font-medium text-lg">{data.date && data.date.hijri.weekday.ar}</p>
+                    <p className="font-light text-md">{data.date && `${ConvertToArabicNumbers(data.date.hijri.day)} ${data.date.hijri.month.ar} ${ConvertToArabicNumbers(data.date.hijri.year)}`}</p>
+                    <p className="font-light text-md">{data.date && data.date.readable}</p>
                 </div>
             </div>
             <form onSubmit={(e) => {
@@ -39,7 +39,7 @@ const Content = () => {
                 <input type='submit' hidden />
             </form>
             <div className="w-72 mx-auto my-4 p-4 border rounded-xl border-gray-300">
-                <ul className='space-y-6'>
+                <ul className='space-y-5'>
                     <li className='flex justify-between'>
                         <p>Sobbu</p>
                         <p>{data.timings && data.timings.Fajr}</p>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import logo from '../assets/mosque.svg'
 
 const Navbar = () => {
     return (
-        <div className='w-full md:px-64 p-4 bg-slate-500 flex justify-between'>
-            <div className='cursor-pointer font-medium text-xl'>Lema Bektoh</div>
+        <div className='w-full md:px-64 p-4 bg-slate-500 flex justify-between items-center'>
+            <div className='flex cursor-pointer font-medium text-xl items-center'>
+                <img src={logo} alt="mosque" className='h-8 pr-1' />
+                Lema Bektoh
+            </div>
             <div>Inpo</div>
         </div>
     )
